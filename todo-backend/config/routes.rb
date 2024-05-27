270ba3606d6ff8144ac3fc7a8a0660c0fc782941
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-05-27 15:48:34 +0900

    todoの一覧実装

--- todo-backend/config/routes.rb
+++ todo-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :todos, except: [:new, :edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
