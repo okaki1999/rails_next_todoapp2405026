0ab6ae374bed8996d2f2b102182b3527abd0f3a5
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-05-30 16:55:30 +0900

    投稿機能のviewの編集

--- todo-frontend/pages/index.tsx
+++ todo-frontend/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Todos from '@/components/Todos';
+import CreateTodoForm from '@/components/CreateTodoForm';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
+    <CreateTodoForm />
     <Todos />
     </div>
   );
